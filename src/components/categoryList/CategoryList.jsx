f497f55ea3127d71424d69c50edb0c6e1c6215d5
fahrizak <webdaata@gmail.com>
2024-07-30 09:49:24 +0700

    auth

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import styles from "./categoryList.module.css";
+import Link from "next/link";
+
+const categories = [
+  "Education",
+  "Kids",
+  "Economy",
+  "Technology",
+  "AI",
+  "Teenager",
+];
 
 const CategoryList = () => {
-  return <div>CategoryList</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Post Categories</h1>
+      <div className={styles.categories}>
+        {categories.map((category) => (
+          <Link
+            key={category}
+            href={`/blog?cat=${category}`}
+            className={`${styles.category} ${styles[category.toLowerCase()]}`}
+          >
+            {category}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryList;
